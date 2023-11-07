e6bed855f46722a160e8c2af3bbc50aa9c22c527
Rupinder Singh <rupindersingh0853@gmail.com>
2023-11-07 22:00:06 +0530

    Added a page component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,15 @@
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <header>
+                <title>Next Reviews</title>
+            </header>
             <body>
                 {children}
             </body>
+            <footer>
+                Footer Here...
+            </footer>
         </html>
     )
 }
