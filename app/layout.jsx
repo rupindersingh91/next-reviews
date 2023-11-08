e378d69e3372963934e131b9432bd44fbd46e9b8
Rupinder Singh <rupindersingh0853@gmail.com>
2023-11-08 21:51:46 +0530

    Minor fixes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,17 @@
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <header>
-                <title>Next Reviews</title>
-            </header>
             <body>
-                {children}
+                <header>
+                    <title>Next Reviews</title>
+                </header>
+                <main>
+                    {children}
+                </main>
+                <footer>
+                    Footer Here...
+                </footer>
             </body>
-            <footer>
-                Footer Here...
-            </footer>
         </html>
     )
 }
