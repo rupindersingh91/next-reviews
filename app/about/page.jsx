5923d70dae058fa5d4054c96b48fd4a547dff6ca
Rupinder Singh <rupindersingh0853@gmail.com>
2023-11-10 06:42:45 +0530

    Added about and 1 more review pge

---
+++ app/about/page.jsx
@@ -0,0 +1,9 @@
+export default function AboutPage(params) {
+    return (
+        <>
+            <h1>About</h1>
+            <p>This is about page for learn next</p>
+        </>
+    )
+
+}
---
+++ app/reviews/hollow-knights/page.jsx
@@ -0,0 +1,10 @@
+export default function HollowKnightsPage() {
+    return (
+        <div>
+            <h1>Hollow Knights</h1>
+            <p>
+                Here will be all reviews of Hollow Knights
+            </p>
+        </div>
+    )
+}
