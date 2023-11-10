d1a869bcc2f6ea4a2a3503fb4cf694bf2d6d6ea7
Rupinder Singh <rupindersingh0853@gmail.com>
2023-11-10 06:35:42 +0530

    Page routes

---
+++ app/reviews/page.jsx
@@ -0,0 +1,13 @@
+export default function ReviewsPage() {
+    return (
+        <>
+            <div>
+                <h1>Reviews</h1>
+                <p>
+                    Here will be all reviews
+                </p>
+            </div>
+        </>
+
+    )
+}
---
+++ app/reviews/stardew-velly/page.jsx
@@ -0,0 +1,13 @@
+export default function StardewVellyPage() {
+    return (
+        <>
+            <div>
+                <h1>Stardew Velly</h1>
+                <p>
+                    Here will be all reviews of Stardew Velly
+                </p>
+            </div>
+        </>
+
+    )
+}
