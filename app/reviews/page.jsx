3d56c2dd18e06eaf6109fdbfd53748faac1ef59a
Rupinder Singh <rupindersingh0853@gmail.com>
2023-11-29 21:48:21 +0530

    Linked the review pages to the parent page

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,8 +1,18 @@
+import Link from "next/link";
+
 export default function ReviewsPage() {
     return (
         <>
             <div>
                 <h1>Reviews</h1>
+                <ul>
+                    <li>
+                        <Link href="/reviews/hollow-knights">Hollow Knights</Link>
+                    </li>
+                    <li>
+                        <Link href="/reviews/stardew-velly">Stardew Valley</Link>
+                    </li>
+                </ul>
                 <p>
                     Here will be all reviews
                 </p>
