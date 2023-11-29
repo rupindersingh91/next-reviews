fc2d62d9bbc185c9e5b3fc2b857050cd448e6e06
Rupinder Singh <rupindersingh0853@gmail.com>
2023-11-29 21:47:46 +0530

    Added link to footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
                                 <Link href="/reviews">Reviews</Link>
                             </li>
                             <li>
-                                <Link href="/about"  prefetch={false}>About</Link>
+                                <Link href="/about" prefetch={false}>About</Link>
                             </li>
                         </ul>
                     </nav>
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
                     {children}
                 </main>
                 <footer>
-                    [footer]
+                    Game data and images courtesy of <a href="https://www.rawg.com" target="_blank">RAWG</a>
                 </footer>
             </body>
         </html>
